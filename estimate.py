fd78de7d6ae325567a774c685c89de06892cf286
adithyasankar47 <adithyasankar47@gmail.com>
2021-05-30 11:41:35 +0530

    Final solution

--- estimate.py
+++ estimate.py
@@ -1,32 +1,58 @@
 import math
 import unittest
+import random
+
+
+def wallis(n):
+    hpi = 1.0
+    for i in range(1, n+1):
+        x = 4.0*(i*i)
+        hpi *= x/(x-1)
+    return 2.0*hpi
+
+
+def monte_carlo(n):
+    c = 0.0
+    for i in range(1, n+1):
+        x = random.random()*random.randrange(-1, 2, 2)
+        y = random.random()*random.randrange(-1, 2, 2)
+        if(x*x+y*y <= 1):
+            c += 1.0
+    pi = 4.0*c/n
+    return pi
+
 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
             pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
-            
+            self.assertTrue(abs(pi - math.pi) > 0.15,
+                            msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
+
     def test_high_iters(self):
         for i in range(500, 600):
             pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+            self.assertTrue(abs(pi - math.pi) < 0.01,
+                            msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
 
 
 class TestMC(unittest.TestCase):
     def test_randomness(self):
         pi0 = monte_carlo(15000)
         pi1 = monte_carlo(15000)
-        
-        self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
 
-        self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
+        self.assertNotEqual(
+            pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
+
+        self.assertFalse(abs(
+            pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
 
     def test_accuracy(self):
         for i in range(500, 600):
             pi = monte_carlo(i)
-            self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
-    
+            self.assertTrue(abs(pi - math.pi) < 0.4,
+                            msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+
+
 if __name__ == "__main__":
     unittest.main()
